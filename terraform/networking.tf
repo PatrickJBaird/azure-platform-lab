8b2b1b91aa8dbe42588cc68c4f2a9af388a5bd7a
Patrick Baird <patrickjbaird@gmail.com>
2026-09-14 21:33:55 +1000

    Seperate functions out of main and Add network security group and subnet association inc ssh port rule

---
+++ terraform/networking.tf
@@ -0,0 +1,13 @@
+resource "azurerm_virtual_network" "lab" {
+  name                = "vnet-lab"
+  address_space       = ["10.0.0.0/16"]
+  location            = azurerm_resource_group.lab.location
+  resource_group_name = azurerm_resource_group.lab.name
+}
+
+resource "azurerm_subnet" "lab" {
+  name                 = "snet-main"
+  resource_group_name  = azurerm_resource_group.lab.name
+  virtual_network_name = azurerm_virtual_network.lab.name
+  address_prefixes     = ["10.0.1.0/24"]
+}
