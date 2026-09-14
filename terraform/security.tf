87ad58abe25d1eda34eccfaa32374ec0fb0718df
Patrick Baird <patrickjbaird@gmail.com>
2026-09-14 23:27:34 +1000

    Deploy nginx using ansible and add rule for http port 80 for vm

--- terraform/security.tf
+++ terraform/security.tf
@@ -21,6 +21,23 @@ resource "azurerm_network_security_rule" "ssh" {
   network_security_group_name = azurerm_network_security_group.main.name
 }
 
+resource "azurerm_network_security_rule" "http" {
+  name      = "Allow-HTTP"
+  priority  = 110
+  direction = "Inbound"
+  access    = "Allow"
+  protocol  = "Tcp"
+
+  source_port_range      = "*"
+  destination_port_range = "80"
+
+  source_address_prefix      = "*"
+  destination_address_prefix = "*"
+
+  resource_group_name         = azurerm_resource_group.lab.name
+  network_security_group_name = azurerm_network_security_group.main.name
+}
+
 resource "azurerm_subnet_network_security_group_association" "main" {
   subnet_id                 = azurerm_subnet.lab.id
   network_security_group_id = azurerm_network_security_group.main.id
